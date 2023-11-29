316272898b9d86f2a68a6202d3fc704bc6abef2b
Fayzul <fayzulhaq0708@gmail.com>
2023-11-29 21:08:29 +0800

    feat: user model

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,128 +1,78 @@
-//package com.example.demo.model;
-//
-//import jakarta.persistence.*;
-////import org.springframework.security.core.GrantedAuthority;
-////import org.springframework.security.core.authority.SimpleGrantedAuthority;
-////import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Entity
-//@Table(name = "user")
-//
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String username;
-//
-//    private String password;
-//
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(  name = "user_roles",
-//            joinColumns = @JoinColumn(name = "user_id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id"))
-//    private Set<Role> roles = new HashSet<>();
-//
-//    public User() {
-//    }
-//
-//    public User(String username, String password) {
-//        this.username = username;
-//        this.password = password;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public Set<Role> getRoles() {
-//        return roles;
-//    }
-//
-//    public void setRoles(Set<Role> roles) {
-//        this.roles = roles;
-//    }
-//}
-//
-////@Entity
-////@Table(name = "user")
-////public class User {
-////    @Id
-////    @GeneratedValue(strategy = GenerationType.AUTO)
-////    private Long id;
-////    private String username;
-////    private String password;
-////    @Enumerated(EnumType.STRING)
-////    private Role role;
-////
-////    public User(Long id, String username, String password) {
-////        this.id = id;
-////        this.username = username;
-////        this.password = password;
-////    }
-////
-////    public String getUsername() {
-////        return username;
-////    }
-//////
-//////    @Override
-//////    public boolean isAccountNonExpired() {
-//////        return true;
-//////    }
-//////
-//////    @Override
-//////    public boolean isAccountNonLocked() {
-//////        return true;
-//////    }
-//////
-//////    @Override
-//////    public boolean isCredentialsNonExpired() {
-//////        return true;
-//////    }
-//////
-//////    @Override
-//////    public boolean isEnabled() {
-//////        return true;
-//////    }
-////
-////    public void setUsername(String username) {
-////        this.username = username;
-////    }
-////
-//////    @Override
-//////    public Collection<? extends GrantedAuthority> getAuthorities() {
-//////        return List.of(new SimpleGrantedAuthority(role.name()));
-//////    }
-////
-////    public String getPassword() {
-////        return password;
-////    }
-////
-////    public void setPassword(String password) {
-////        this.password = password;
-////    }
-////}
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    private String role = "USER"; // Default role
+
+    public User(int id, String email, String username, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String email, String username, String password, String role) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

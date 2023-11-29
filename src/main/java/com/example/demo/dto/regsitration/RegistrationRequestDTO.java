0763e145df6e59c478840c329567850f41e41a6b
Fayzul <fayzulhaq0708@gmail.com>
2023-11-29 21:07:17 +0800

    feat: register and login dto

---
+++ src/main/java/com/example/demo/dto/regsitration/RegistrationRequestDTO.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto.regsitration;
+public class RegistrationRequestDTO {
+    private String email;
+    private String password;
+
+    private String username;
+
+    public RegistrationRequestDTO(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public RegistrationRequestDTO() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
